004d1b21e77da1a531c016ef632bc7b88909da38
Alexander Rozhdestvenskiy <ppcrn97@gmail.com>
2023-05-26 12:52:55 +0300

    create ClientList

---
+++ Departments/Client.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Departments
+{ 
+    internal class Client : IComparable<Client>
+    {
+
+        private static int staticId;
+
+        static Client()
+        {
+            staticId = 0;
+        }
+
+        private static int NextId()
+        {
+            staticId++;
+            return staticId;
+        }
+
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Secondname { get; set; }
+        public string Phone { get; set; }
+        public string Passport { get; set; }
+
+        /// <summary>
+        /// Создание клиента
+        /// </summary>
+        /// <param name="DepartmentId">ID департамента</param>
+        /// <param name="Surname">Фамилия</param>
+        /// <param name="Name">Имя</param>
+        /// <param name="Secondname">Отчество</param>
+        /// <param name="Phone">Телефон</param>
+        /// <param name="Passport">Паспорт</param>
+        public Client(int DepartmentId, string Surname, string Name, string Secondname, string Phone, string Passport)
+        {
+            this.Id = NextId();
+            this.DepartmentId = DepartmentId;
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Secondname = Secondname;
+            this.Phone = Phone;
+            this.Passport = Passport;
+        }
+
+        public JObject Serialize()
+        {
+            JObject jObject = new JObject();
+            jObject["Id"] = Id;
+            jObject["DepartmentId"] = DepartmentId;
+            jObject["Surname"] = Surname;
+            jObject["Name"] = Name;
+            jObject["Secondname"] = Secondname;
+            jObject["Phone"] = Phone;
+            jObject["Passport"] = Passport;
+
+            return jObject;
+        }
+
+        public int CompareTo(Client other)
+        {
+            return String.Compare(this.Surname, other.Surname);
+        }
+    }
+}
---
+++ Departments/ClientList.cs
@@ -0,0 +1,260 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Departments
+{
+    internal class ClientList : IList<Client>
+    {
+        private Client[] clients;
+
+
+        #region IList inheritance 
+
+
+        /// <summary>
+        /// Экземпляр Client, возвращает null если такого нет
+        /// </summary>
+        /// <param name="index">Позиция в листе</param>
+        /// <returns>Client</returns>
+        public Client this[int index]
+        {
+            get
+            {
+                return this.clients[index];
+            }
+            set
+            {
+                this.clients[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Число элементов в массиве
+        /// </summary>
+        public int Count => this.clients.Length;
+
+        public bool IsReadOnly 
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(Client item)
+        {
+            if(clients == null)
+            {
+                clients = new Client[1];
+                clients[0] = item;
+            }
+            else
+            {
+                Client[] newList = new Client[clients.Length + 1];
+                for(int i = 0; i < clients.Length; i++)
+                {
+                    newList[i] = this.clients[i];
+                }
+                newList[newList.Length - 1] = item;
+                clients = newList;
+            }
+        }
+
+        public void Clear()
+        {
+            for(int i = 0; i < clients.Length; i++)
+            {
+                clients[i] = null;
+            }
+            clients = null;
+        }
+
+        public bool Contains(Client item)
+        {
+            for(int i = 0; i < clients.Length; i++)
+            {
+                if(clients[i].Equals(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(Client[] array, int arrayIndex)
+        {
+            for(int i = 0; i < clients.Length; i++)
+            {
+                array[arrayIndex] = clients[i];
+                arrayIndex++;
+            }
+        }
+
+        public IEnumerator<Client> GetEnumerator()
+        {
+            return new ClientEnumerator(clients);
+        }
+
+        public int IndexOf(Client item)
+        {
+            for(int i = 0; i < clients.Length; i++)
+            {
+                if (clients[i].Equals(item))
+                    return i;
+            }
+            return -1;
+        }
+
+        public void Insert(int index, Client item)
+        {
+            if(index >= 0 && index <= clients.Length)
+            {
+                Client[] newList = new Client[clients.Length + 1];
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    if(i < index)
+                        newList[i] = this.clients[i];
+                    else 
+                        newList[i + 1] = this.clients[i];
+                }
+                newList[index] = item;
+                clients = newList;
+            }
+        }
+
+        public bool Remove(Client item)
+        {
+            int position = IndexOf(item);
+            if (position != -1)
+            {
+                RemoveAt(position);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < clients.Length))
+            {
+                Client[] newList = new Client[clients.Length - 1];
+
+                for (int i = 0; i < clients.Length - 1; i++)
+                {
+                    if (i < index)
+                        newList[i] = this.clients[i];
+                    else
+                        newList[i] = this.clients[i + 1];
+                }
+                clients = newList;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public class ClientEnumerator : IEnumerator<Client>
+        {
+            private Client[] data;
+            private int pos = -1;
+
+            public ClientEnumerator(Client[] data)
+            {
+                this.data = data;
+            }
+
+            public bool MoveNext()
+            {
+                pos++;
+                return (pos < data.Length);
+            }
+
+            public void Reset()
+            {
+                pos = -1;
+            }
+
+            public void Dispose()
+            {
+                for(int i = 0; i < data.Length; i++)
+                {
+                    data[i] = null;
+                }
+                data = null;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (pos == -1 || pos >= data.Length)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    return data[pos];
+                }
+            }
+
+            Client IEnumerator<Client>.Current
+            {
+                get
+                {
+                    if (pos == -1 || pos >= data.Length)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    return data[pos];
+                }
+            }
+        }
+
+        #endregion
+
+        public void Deserialize(string path)
+        {
+            string str = "";
+
+            try
+            {
+                str = File.ReadAllText(path);
+            }
+            catch
+            {
+                MessageBox.Show("File does not open");
+            }
+
+            clients = JsonConvert.DeserializeObject<Client[]>(str);
+        }
+
+        public void Serialize(string path)
+        {
+            JArray array = new JArray();
+
+            foreach (Client client in clients)
+                array.Add(client.Serialize());
+
+            string str = array.ToString();
+
+            try
+            {
+                File.WriteAllText(path, str);
+            }
+            catch
+            {
+                MessageBox.Show("File does not open");
+            }
+        }
+
+
+    }
+}
